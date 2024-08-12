897e618bdfba1f08572e36abd0141e735e739c94
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-08-12 06:26:05 +0545

    added defaultProps for the student

--- my-react-app/src/Student.jsx
+++ my-react-app/src/Student.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function Student(props) {
   return (
-    <div>
+    <div className="student">
       <p>
         My Name is: {props.name} and I am {props.age}. And I am a Student.
         {props.isStudent ? "Yes" : "no"}
@@ -14,6 +14,12 @@ Student.propTypes = {
   name: PropTypes.string.isRequired,
   age: PropTypes.number.isRequired,
   isStudent: PropTypes.bool.isRequired,
+};
+
+Student.defaultProps = {
+  name: "Guest",
+  age: 1000,
+  isStudent: "false",
 };
 
 export default Student;
