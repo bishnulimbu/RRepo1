bfba91d7626029b42c5250560aed1ada2bd05c02
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-08-12 06:25:37 +0545

    updated app.jsx for the props-types and such

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,30 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+import Card from "./Card.jsx";
+import Student from "./Student.jsx";
+import Greeting from "./Greeting.jsx";
+
+function App() {
+  return (
+    <>
+      <Greeting isLoggedIn={true} username="ram" />
+      <Greeting isLoggedIn={111} username="ram" />
+      {/* <Student name="ramu Sam" age={100} isStudent={true} /> */}
+      {/* <Student name="ramu Sam" age={100} isStudent={true} /> */}
+      {/* <Student name="ramu Sam" age={100} isStudent={true} /> */}
+      {/* <Student name="ramu Sam" age={100} isStudent={true} /> */}
+      {/* <Student name="ramu Sam" age={100} isStudent={true} /> */}
+      {/* <Student /> */}
+      {/* <Header /> */}
+      {/* <Food /> */}
+      {/* <Card /> */}
+      {/* <Card /> */}
+      {/* <Card /> */}
+      {/* <Card /> */}
+      {/* <Footer />; */}
+    </>
+  );
+}
+
+export default App;
