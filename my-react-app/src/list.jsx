c173c7f22bcd0c3c571d2035028222d8f0d352a1
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-09-01 20:59:02 +0545

    updated listing

--- my-react-app/src/list.jsx
+++ my-react-app/src/list.jsx
@@ -1,5 +1,6 @@
 function List(props) {
   const newList = props.appList;
+  const category = props.category;
   // list1.sort((a, b) => a.name.localeCompare(b.name));
 
   // newList.sort((a, b) => a.name.localeCompare(b.name));
@@ -9,14 +10,23 @@ function List(props) {
     // <li key={list.key}>
     // key should be used for unique id for same data rendering or so.
     <li key={list.age}>
-      {list.name} : {list.age}
+      {list.name} : <b>{list.age}</b>
       {/* @nbsp */}
       {/* can be used for not breaking space while adding new html tag or so. */}
       {/* this is probably the last of it and last of it for me also probably. yes */}
       {/* probably. */}
     </li>
   ));
-  return <ol>{list2}</ol>;
+  return (
+    <>
+      <h1 className="cat">{category}</h1>
+      <ol className="dog">{list2}</ol>
+    </>
+  );
 }
-
+List.defaultProps = {
+  category: "category",
+  appList: [],
+  //as map will not run with undefined value, so an empty array is set at default for it value.
+};
 export default List;
