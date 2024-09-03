c9065cb675a911f46c5a2133e38e908f013bfa0a
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-09-03 14:51:15 +0545

    added event onchange for number

--- my-react-app/src/Component2.jsx
+++ my-react-app/src/Component2.jsx
@@ -2,13 +2,21 @@ import React, { useState } from "react";
 
 function Component2() {
   const [name, setName] = useState("");
+  const [num, setNum] = useState();
+
   const fn1 = (event) => {
     setName(event.target.value);
+  };
+  const numFn = (event) => {
+    setNum(event.target.value);
   };
   return (
     <div>
       <input type="text" onChange={fn1} />
       <p>your name is: {name}</p>
+
+      <input type="number" onChange={numFn} />
+      <p>Quantity:{num}</p>
     </div>
   );
 }
