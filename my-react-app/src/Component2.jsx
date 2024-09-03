f0cfa584323a922388051a695e576e3d0bcce164
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-09-03 16:46:27 +0545

    full updated onChange usuage example

--- my-react-app/src/Component2.jsx
+++ my-react-app/src/Component2.jsx
@@ -4,6 +4,8 @@ function Component2() {
   const [name, setName] = useState("");
   const [num, setNum] = useState();
   const [area, setArea] = useState("");
+  const [city, setCity] = useState("");
+  const [payment, setPayment] = useState("");
 
   const fn1 = (event) => {
     setName(event.target.value);
@@ -14,6 +16,13 @@ function Component2() {
   const areaFn = (event) => {
     setArea(event.target.value);
   };
+  const dnFn = (event) => {
+    setCity(event.target.value);
+  };
+  const radioFn = (event) => {
+    setPayment(event.target.value);
+  };
+
   return (
     <div>
       <input type="text" onChange={fn1} />
@@ -23,6 +32,37 @@ function Component2() {
       <p>Quantity:{num}</p>
 
       <input type="textarea" onChange={areaFn} placeholder="write something" />
+      <p>Your Comment:{area}</p>
+
+      <br />
+      <br />
+      <select onChange={dnFn}>
+        <option value="">City</option>
+        <option value="kathmandu">kathmandu</option>
+        <option value="kerabari">kerabari</option>
+        <option value="biratnagar">biratnagar</option>
+      </select>
+      <p>City:{city}</p>
+
+      <label>
+        <input
+          type="radio"
+          value="one"
+          checked={payment === "one"}
+          onChange={radioFn}
+        />
+        One
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="two"
+          checked={payment === "two"}
+          onChange={radioFn}
+        />
+        Two
+      </label>
+      <p>Payment:{payment}</p>
     </div>
   );
 }
