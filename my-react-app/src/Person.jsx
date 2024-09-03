1f6c33fb330756c1f1b5c7d42345ebb054e31b9e
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-09-03 20:57:01 +0545

    usuage of state object

---
+++ my-react-app/src/Person.jsx
@@ -0,0 +1,32 @@
+import react, { useState } from "react";
+
+function Person() {
+  const [person, setPerson] = useState(
+    { name: "ram" },
+    { age: 18 },
+    { gender: "male" },
+  );
+  function nameFn(event) {
+    setPerson((p) => ({ ...p, name: event.target.value }));
+  }
+  function ageFn(event) {
+    setPerson((p) => ({ ...p, age: event.target.value }));
+  }
+  function genderFn(event) {
+    setPerson((p) => ({ ...p, gender: event.target.value }));
+  }
+
+  return (
+    <>
+      <p>
+        My name is: {person.name}, of age: {person.age} and of gender:{" "}
+        {person.gender}
+      </p>
+      <input type="text" placeholder="name" onChange={nameFn} />
+      <input type="number" placeholder="age" onChange={ageFn} />
+      <input type="text" placeholder="gender" onChange={genderFn} />
+    </>
+  );
+}
+
+export default Person;
