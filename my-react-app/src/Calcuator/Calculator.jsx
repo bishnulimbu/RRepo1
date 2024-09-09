fdfa58c3665ab5ef66667ed873132cf19f08e3a6
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-09-09 18:46:43 +0545

    caculator without eval

--- my-react-app/src/Calcuator/Calculator.jsx
+++ my-react-app/src/Calcuator/Calculator.jsx
@@ -2,68 +2,173 @@ import React, { useState, useEffect } from "react";
 import "./Calculator.css";
 
 const Calculator = () => {
-  const [data, setData] = useState("");
-  const inputHandler = (e) => {
-    setData(e.target.value);
-  };
+  //will need like four state
+  const [currentNum, setCurrentNum] = useState("0");
+  const [prevNum, setPrevNum] = useState("");
+  const [operator, setOperator] = useState();
+  const [result, setResult] = useState("");
 
-  function numData(num) {
-    setData((d) => d + num);
-    console.log(data);
+  function inputHandler(e) {
+    setCurrentNum(e.target.value);
   }
-  function addFn() {
-    try {
-      setData(eval(data).toString());
-    } catch {
-      setData("Error");
+
+  function handleNumberClick(num) {
+    setCurrentNum(currentNum + num);
+  }
+
+  function clear() {
+    setCurrentNum("0");
+    setPrevNum("");
+    setOperator(null);
+    setResult(null);
+  }
+
+  function handleOperatorClick(op) {
+    setPrevNum(parseFloat(currentNum)); //setting the number to previous value for operation withe the next value
+    setOperator(op);
+    console.log(operator);
+    inputHandler(currentNum + operator);
+    setCurrentNum(""); //freeing the current num for next number
+  }
+
+  function calculate() {
+    let current = parseFloat("currentNum");
+    let newResult; //temoparay result varaible
+    switch (operator) {
+      case "+":
+        newResult = prevNum + current;
     }
+    setResult(newResult);
+    currentNum(toString(newResult));
+    prevNum("");
+    operator(null);
   }
-
-  useEffect(() => {});
 
   return (
     <div className="calculator">
       <input
         type="text"
-        value={data}
+        value={currentNum}
         className="calculator-input"
-        onChange={(e) => inputHandler(e)}
+        onChange={inputHandler}
       />
       <br />
       <br />
       <div className="calculator-row">
-        <button className="calculator-button">C</button>
-        <button className="calculator-button">+/-</button>
-        <button className="calculator-button">%</button>
-        <button className="calculator-button">/</button>
+        <button className="calculator-button" onClick={clear}>
+          C
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleOperatorClick("+/-")}
+        >
+          +/-
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleOperatorClick("%")}
+        >
+          %
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleOperatorClick("/")}
+        >
+          /
+        </button>
       </div>
       <div className="calculator-row">
-        <button className="calculator-button">7</button>
-        <button className="calculator-button">8</button>
-        <button className="calculator-button">9</button>
-        <button className="calculator-button">*</button>
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick("7")}
+        >
+          7
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick("8")}
+        >
+          8
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick("9")}
+        >
+          9
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleOperatorClick("*")}
+        >
+          *
+        </button>
       </div>
       <div className="calculator-row">
-        <button className="calculator-button">4</button>
-        <button className="calculator-button">5</button>
-        <button className="calculator-button">6</button>
-        <button className="calculator-button">-</button>
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick("4")}
+        >
+          4
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick("5")}
+        >
+          5
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick("6")}
+        >
+          6
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleOperatorClick("-")}
+        >
+          -
+        </button>
       </div>
       <div className="calculator-row">
-        <button className="calculator-button" onClick={() => numData(1)}>
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick("1")}
+        >
           1
         </button>
-        <button className="calculator-button">2</button>
-        <button className="calculator-button">3</button>
-        <button className="calculator-button" onClick={() => numData("+")}>
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick("2")}
+        >
+          2
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick("3")}
+        >
+          3
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleOperatorClick("+")}
+        >
           +
         </button>
       </div>
       <div className="calculator-row">
-        <button className="calculator-button">0</button>
-        <button className="calculator-button">.</button>
-        <button className="calculator-button" onClick={addFn}>
-          {/* "=" button shouldn't immediately run the function so passing the reference without invoking it.   */}
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick("0")}
+        >
+          0
+        </button>
+        <button
+          className="calculator-button"
+          onClick={() => handleNumberClick(".")}
+        >
+          .
+        </button>
+        <button className="calculator-button" onClick={calculate}>
           =
         </button>
       </div>
